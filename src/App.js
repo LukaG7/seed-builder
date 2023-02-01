45b28266b9f88f49db3a7afab96edad5a6389f71
Lukas Gonzalez <lukegonz47@gmail.com>
2023-02-01 15:00:13 -0500

    created signin and signup components with forms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import ResponsiveAppBar from './components/AppBar';
+import { Route, Routes, Link } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
+      <Routes>
+        <Route path='/signin' element={<SignIn />}/>
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
     </div>
   );
 }
